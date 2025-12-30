c057b423f02bdaea08d5811259a8debc7ff6d3ef
ivan_git <paancho10@gmail.com>
2025-12-29 22:05:10 -0300

    Merge pull request #1 from peterCocho/feature/bootstrap Titulo: implementacion base del backend y persistencia feat: implementación de API de predicción y persistencia en PostgreSQL - Creado el flujo completo de backend (Controller, Service, DTO, Model) - Configurada la conexión a base de datos y repositorio JPA - Implementado endpoint de estadísticas para el Dashboard Feature/bootstrap

---
+++ backend-api/src/main/java/com/churninsight/backend_api/dto/PredictionResponse.java
@@ -0,0 +1,29 @@
+package com.churninsight.backend_api.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * DTO que representa la respuesta de la predicción enviada al cliente (Frontend).
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PredictionResponse {
+
+    @JsonProperty("customer_id")
+    private String customerId;
+
+    @JsonProperty("prevision")
+    private String prevision; // Ejemplo: "Va a cancelar" / "Va a continuar"
+
+    @JsonProperty("probabilidad")
+    private Double probabilidad; // Ejemplo: 0.85
+
+    @JsonProperty("status")
+    private String status; // Ejemplo: "Success"
+}
