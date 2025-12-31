47ef7f158ed83a7bfbe1cb05fea406536e0a492c
ivan_git <paancho10@gmail.com>
2025-12-31 16:31:30 -0300

    docs: completar documentacion swagger y comentarios clean code

--- backend-api/src/main/java/com/churninsight/backend_api/service/PredictionServiceImpl.java
+++ backend-api/src/main/java/com/churninsight/backend_api/service/PredictionServiceImpl.java
@@ -4,20 +4,39 @@ import com.churninsight.backend_api.dto.PredictionRequest;
 import com.churninsight.backend_api.dto.PredictionResponse;
 import com.churninsight.backend_api.model.Customer;
 import com.churninsight.backend_api.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 public class PredictionServiceImpl implements PredictionService{
 
     private final CustomerRepository customerRepository;
+    private final WebClient webClient;
+
+    @Value("${api.ds.url}")
+    private String dsApiUrl;
 
     // Inyectamos el repositorio para poder guardar en la DB
-    public PredictionServiceImpl(CustomerRepository customerRepository) {
+    public PredictionServiceImpl(CustomerRepository customerRepository, WebClient webClient) {
         this.customerRepository = customerRepository;
+        // Para las escuchas externas http
+        this.webClient = webClient;
     }
 
     @Override
     public PredictionResponse processPrediction(PredictionRequest request) {
+        // --- PROXIMAMENTE: INTEGRACION REAL ---
+        // Descomenta este bloque cuando la API de Python esté lista:
+        /*
+        PredictionResponse dsResponse = webClient.post()
+            .uri(dsApiUrl)
+            .bodyValue(request)
+            .retrieve()
+            .bodyToMono(PredictionResponse.class)
+            .block(); // Espera la respuesta de forma sincrónica
+        */
+
         // 1. Simular la lógica del modelo (esto luego será la llamada a Python)
         String prevision = (request.getLastLoginDays() > 20) ? "Va a cancelar" : "Va a continuar";
         double probabilidad = (prevision.equals("Va a cancelar")) ? 0.88 : 0.12;
